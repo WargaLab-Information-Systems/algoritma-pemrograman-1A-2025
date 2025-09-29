151adcc3df6a3b4592d144575c9fa037ac44cce5
HariSafiraSetyawati <harisafirasetyawati@gmail.com>
2025-09-29 18:41:59 +0700

    new file:   modul-1/250441100028_HariSafiraSetyawati/Jawaban Soal 1.py new file:   modul-1/250441100028_HariSafiraSetyawati/Jawaban Soal 2.py new file:   modul-1/250441100028_HariSafiraSetyawati/Jawaban Soal 3.py new file:   modul-1/250441100028_HariSafiraSetyawati/Praktikum 1.py new file:   modul-1/LAPRES MODUL 1.docx

---
+++ modul-1/250441100028_HariSafiraSetyawati/Praktikum 1.py
@@ -0,0 +1,47 @@
+# a = 5 + 3
+# b = 5 - 3
+# c = 5 * 3
+# d = 6 / 3
+# e = 15 // 4
+# f = 2 ** 4
+# g = 19 % 4
+
+# print("hasil pertambahan", a)
+# print("hasil pengurangan", b)
+# print("hasil perkalian", c)
+# print("hasil pembagian", d)
+# print("hasil pembagian bulat", e)
+# print("hasil pangkat", f)
+# print("hasil sisa bagi", g)
+
+# a, b, c = 10, 5, 2
+# print(a+b)
+
+# nama = "Agus pergi ke pasar"
+# (print(nama))
+
+# nilai = 25
+# print("nilai saya adalah", + str(nilai))
+# print("nilai saya adalah", nilai)
+
+# nilai = 3.14
+# print("nilai phi adalah", nilai)
+
+# total_harga = 150000
+# diskon = 0.1
+# harga_akhir = total_harga - (total_harga * diskon)
+# print(harga_akhir)
+
+# nama = "Budi"
+# umur = 19
+# print(nama + " berumur" + str (umur) + " tahun")
+
+harga_beli = 25000
+harga_jual = 30000
+kg = 12
+modal = harga_beli*kg
+pendapatan = harga_jual*kg
+untung = pendapatan - modal
+print("modalnya sebesar: ", modal)
+print("pendapatannya sebesar: ", pendapatan)
+print("keuntungannya sebesar: ", untung)
