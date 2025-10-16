7222b9a9998f098b816d5aa378b6d10648c8b7bf
kiki <kikinigenk48@gmail.com>
2025-10-16 19:50:08 +0700

    250441100098_MRizqiHidayatulloh_AsprakKakDestya

---
+++ UTS/250441100098_MRizqiHidayatulloh_AsprakKakDestya/250441100098MRizqiHidayatullohUTS.py.py
@@ -0,0 +1,20 @@
+#MENGHITUNG HARGA
+Beras = 12.000
+Gula = 14.000
+Minyak = 20.000
+
+while (Beras,Gula,Minyak):
+    print ("Lombok,bawang")
+
+Pelanggan_yang_memiliki_member_card = 'Pelanggan_yang_memiliki_member_card mendapatkan diskon 5%'
+Total_belanja =int(input('mendapat tambahan diskon 5% dari total akhir.' ))
+
+if Total_belanja >= 100000 :
+    print ("Mendapatkan diskon 10%")
+elif Total_belanja >= 200000 :
+    print ("Mendapatkan diskon 20%")
+elif Pelanggan_yang_memiliki_member_card == "Memiliki member card":
+    print("mendapat tambahan diskon 5% dari total akhir. ")
+else: 
+    print("Tidak mendapatkan diskon")
+
---
+++ UTS/250441100098_MRizqiHidayatulloh_AsprakKakDestya/readme.md
@@ -0,0 +1,3 @@
+disini nomer 1 membuat variable harga dan nomer 2 ada perulangan untuk menambah item yang di beli oleh pelanggan dan lanjut untuk yang membeli di atas 100000 mendapatkan diskon 10% dan jika berbelanja 200000 mendapatankan 200000
+lanjut untuk yang memiliki member card mendapatkan diskon juga 5% di situ ada penyeleksian kondisi jika benar dapet diskon 
+
