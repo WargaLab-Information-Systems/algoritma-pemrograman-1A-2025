c78df076bd934ae5913f20a647f430337560adea
250441100106crysolitiara <crysolisimanjuntak@gmail.com>
2025-11-03 15:27:11 +0700

    250441100106_CrysoliTiaraSimanjuntak_Modul4_AsprakKakDesty

---
+++ modul-4/250441100106_CrysoliTiaraSimanjuntak_Modul5_AsprakKakDestya/soal no 3.py
@@ -0,0 +1,16 @@
+# soal no 3
+
+n = int(input("Masukkan nilai n: ")) 
+
+for i in range(n, 0, -1):
+
+    for j in range(1, i + 1):
+        print(j, end="")
+
+    space = (n - i) * 2
+    print(" " * space, end="")                                 
+
+    for j in range(i,0, -1):
+        print(j, end="")
+
+    print() 
