c0d2ff9b30ee1f31bc97689c00237417542a1b63
HariSafiraSetyawati <harisafirasetyawati@gmail.com>
2025-11-04 18:18:18 +0700

    modified:   modul-5/250441100028_HariSafiraSetyawati/Jawaban Soal 1.py

--- modul-5/250441100028_HariSafiraSetyawati/Jawaban Soal 1.py
+++ modul-5/250441100028_HariSafiraSetyawati/Jawaban Soal 1.py
@@ -1,15 +1,9 @@
-# Program menghitung faktorial secara rekursif
-# Kasus: Pak Tungtung ingin membuka brankas tua yang membutuhkan perhitungan faktorial
-
 def faktorial(n):
-    # Basis rekursi: jika n = 0 atau n = 1, maka faktorialnya adalah 1
     if n == 0 or n == 1:
         return 1
     else:
-        # Pemanggilan rekursif
         return n * faktorial(n - 1)
 
-# Program utama (dinamis, menerima input dari pengguna)
 try:
     N = int(input("Masukkan bilangan bulat non-negatif (N): "))
     
