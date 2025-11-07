b92974608db872c8f414569458e5fe64c01827d5
syainulpratamal-crypto <syainulpratamal@gmail.com>
2025-11-07 09:55:54 +0700

    250441100004_AchSyainulPratama_Modul5_kak_yahya

---
+++ 250441100004_AchSyainulPratama_modul5_kak_yahya/modul5.py
@@ -0,0 +1,8 @@
+def faktorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * faktorial(n - 1)
+
+angka = int(input("Masukkan bilangan: "))
+print(f"Faktorial dari {angka} adalah {faktorial(angka)}")  
