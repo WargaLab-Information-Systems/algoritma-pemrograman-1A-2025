ca0dfe8df53bfdd42c718fc0bcc25a32f2b3a2d7
wima09 <wimasapta6@gmail.com>
2025-11-18 18:43:32 +0700

    Fix output formatting and adjust spacing in pattern generation

--- modul-4/250441100082_WimaSeptaAbiFadana_AsprakDestyaNurfaizaMuslim/250441100082_WimaSeptaAbiFadana_soal_no_3.py
+++ modul-4/250441100082_WimaSeptaAbiFadana_AsprakDestyaNurfaizaMuslim/250441100082_WimaSeptaAbiFadana_soal_no_3.py
@@ -2,9 +2,9 @@ n = int(input("masukan jumlah barisnya : "))
 
 for i in range(n, 0, -1):
     for j in range(1, i + 1):
-        print(j, end="*")
+        print(j, end=" ")
 
-    for k in range((n-i)):
+    for k in range(2 * (n-i)):
         print(" ", end=" ")
 
     for p in range(i, 0, -1):
